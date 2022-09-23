a22c2095c255bfeb0ccd4729a81134622a911d7f
yehj <hjye2@iflytek.com>
2022-09-23 14:36:36 +0800

    stylelint

--- vite-react-ts/.stylelintrc.cjs
+++ vite-react-ts/.stylelintrc.cjs
@@ -3,8 +3,9 @@ module.exports = {
   plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'], //, 'stylelint-config-rational-order'
   rules: {
-    'at-rule-empty-line-before': 'always' | 'never',
-    'at-rule-name-case': 'lower' | 'upper',
+    // 'at-rule-empty-line-before': 'always' | 'never',
+    // 'at-rule-name-case': 'lower' | 'upper',
+    'selector-class-pattern': null,
     'block-no-empty': true,
     'selector-pseudo-class-no-unknown': [
       true,
@@ -12,18 +13,19 @@ module.exports = {
         ignorePseudoClasses: ['global'],
       },
     ],
-    'comment-empty-line-before': false,
-    'value-list-comma-newline-after': false,
-    'declaration-colon-newline-after': false,
-    'no-descending-specificity': false,
+    'comment-empty-line-before': null,
+    'value-list-comma-newline-after': null,
+    'declaration-colon-newline-after': null,
+    'no-descending-specificity': null,
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['mixin', 'include'],
       },
     ],
-    'no-empty-source': false,
-    'rule-empty-line-before': false,
+    'no-empty-source': null,
+    'rule-empty-line-before': null,
+    'property-no-unknown': null,
   },
   ignoreFiles: ['node_modules/**/*', 'dist/**/*'],
 };
