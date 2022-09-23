38b2ece19f6b44d097622aecab028058bcb128d3
yehj <hjye2@iflytek.com>
2022-09-23 13:19:06 +0800

    prettier

--- vite-react-ts/prettier.config.cjs
+++ vite-react-ts/prettier.config.cjs
@@ -15,9 +15,9 @@ module.exports = {
   // jsx 不使用单引号，而使用双引号
   // jsxSingleQuote: false,
   // 末尾不需要逗号
-  // trailingComma: 'es5',
+  trailingComma: 'all',
   // 大括号内的首尾需要空格
-  // bracketSpacing: true,
+  bracketSpacing: true,
   // jsx 标签的反尖括号需要换行
   // jsxBracketSameLine: false,
   // 箭头函数，只有一个参数的时候，也需要括号
