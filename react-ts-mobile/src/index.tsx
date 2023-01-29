55598cb6a7bb70c926a8a01d60b5a694f83f5dd8
yehj <hjye2@iflytek.com>
2023-01-29 11:00:25 +0800

    升级框架

--- react-ts-mobile/src/index.tsx
+++ react-ts-mobile/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import '@assets/css/common.scss';
 // import bg from '@assets/images/no-img.png';
 // icon字体样式
 import '@assets/font/icon-fonts/icon.css';
-import ReactDom from 'react-dom';
 import App from '@pages/App';
 import 'core-js';
 // import env from '@config';
 // console.log(env);
 // import style from './index.module.scss';
-
-ReactDom.render(<App />, document.querySelector('#app'));
+const container: Element = document.getElementById('app') as Element;
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
