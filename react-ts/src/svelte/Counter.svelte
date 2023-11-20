05fcaff0a9e04cd2a6814549a2c79f4a313c66c1
yehj <hjye2@iflytek.com>
2023-11-20 16:52:49 +0800

    计算属性

--- react-ts/src/svelte/Counter.svelte
+++ react-ts/src/svelte/Counter.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import Counter from './Counter1.svelte';
   let count: number = 0
-  export let a: Number; // 参数
+  export let a: string; // 参数
   export let click: () => void; // 参数
   // const increment = () => {
   //   count += 1
   // }
+  $:len = a.length;
 </script>
 
 <button class="button" on:click={click}>
   count is {count}
 </button>
 <div>{a}</div>
+<div>{len}</div>
 <Counter>12133</Counter>
 <style>
   .button {
