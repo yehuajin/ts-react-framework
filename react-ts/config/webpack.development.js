f3a423172bbab5863970f83e4f7590dc219f4b67
yehj <hjye2@iflytek.com>
2024-02-02 10:48:12 +0800

    升级webpack-dev-server

--- react-ts/config/webpack.development.js
+++ react-ts/config/webpack.development.js
@@ -21,17 +21,10 @@ const config = {
   },
   devServer: {
     historyApiFallback: true, // 刷新页面会响应到index页面，避免出现404
-    contentBase: join(__dirname, `../${configIndex.assets}`), // 资源文件目录
-    inline: true, // 模式 iframe，监听文件变化，自动刷新页面
-    // host: '0.0.0.0', // 使用ip访问
     port: prot,
-    // node-notifier, webpack-build-notifier
-    quiet: true, // 配合friendly-error-webpack-plugin
-    watchContentBase: true,
-    stats: {
-      errorDetails: true,
-    },
     hot: true,
+    liveReload: false,
+    static: join(__dirname, `../${configIndex.assets}`),
     proxy: {
       '/app/**': {
         target: 'https://127.0.0.1/',
